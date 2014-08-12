39e661daafe35ced93fdd2284cbc55c83dee918f
Xavier Badosa <xbadosa@gmail.com>
2014-08-12 19:52:52 +0200

    visual.setup.js for gh-pages

---
+++ pages/visual.setup.js
@@ -0,0 +1,383 @@
+/*
+Copyright (c) 2014 Institut d'Estadistica de Catalunya (Idescat)
+http://www.idescat.cat (https://github.com/idescat/visual)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+VisualJS.setup={ //v.0.9.3
+	//Colors for maps and series
+	colors: {
+		map: {
+			max: 100, //If not enough colors, legend is deceiving and it's better to remove it
+			base: "#09111a"
+		},
+		series: ["#2b527b", "#a52a2a", "#008000", "#ffbf00", "#5959ff", "#ff5959", "#9acd32", "#af8d26"]
+	},
+	//Default options (They can be dynamically modified thru visual().)
+	canvas: {
+		unit : {
+			label: "", 
+			symbol: "",
+			position: "end"
+		},
+		legend: true,
+		grid: {
+			border: 0, //grid border width
+			shadow: 4, //line shadow width
+			line: 2, //line width
+			point: 1 //point radius
+		},
+		axis: { //show axes?
+			x: true,
+			y: true
+		},
+		dec: null, //Show only needed decimals (remove ending zeros) unless (recommended) valid dec has been specified by user
+		autoheading: true,
+
+		//Arrays are not accepted here. "bar", "tsline" and "tsbar" currently don't accept a number.
+		range: {
+			//Quantile. No filtering: 0
+			cmap: 0.05, //Used in color assignation in maps
+
+			//Multiplier. No filtering: 1
+			rank: 1.02, //Increase area horizontally by 2% of the longest bar
+			pyram: 1.02 //Increase area horizontally by 2% of the longest bar
+		}
+	},
+
+	//Internationalization options
+	i18n: {
+		lang: "ca", //default lang when no lang is specified
+		text: {
+			dec: { //decimal separator
+				ca: ",",
+				es: ",",
+				en:  "."
+			},
+			k: { //thousands separator
+				ca: ".",
+				es: ".",
+				en:  ","
+			},
+			month: { //Month axis labels
+				ca: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
+				es:  ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+				en:  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			},
+			quarter: { //Quarter axis labels
+				ca: ["I", "II", "III", "IV"],
+				es:  ["I", "II", "III", "IV"],
+				en:  ["Q1", "Q2", "Q3", "Q4"]
+			},
+			na: { //text in tooltip when value is not available 
+				ca: "Valor no disponible",
+				es: "Valor no disponible",
+				en:  "Value not available"
+			},
+			oldbrowser: { //Warning message when IE<9 (maps)
+				ca: "Per visualitzar el mapa cal un navegador m&eacute;s modern.",
+				es: "Para visualizar el mapa es preciso un navegador m&aacute;s moderno.",
+				en:  "To view the map you must use a modern browser."
+			}
+		}
+	},
+
+	//Classes and ids of elements created by visual
+	id: "visual", //id to style the container
+	clas: "visual", //class to style the container
+	compareids: ["VisualJSleft", "VisualJSright"], //ids to style each VisualJS.compare containers
+	tooltipid: "VisualJSTooltip", //id to style the tooltip
+	nowrapclass: "VisualJSnw", //class to define blocks of wrappable content in the title
+	canvasclass: "VisualJScanvas", //canvas container (Flot)
+	areaclass: "VisualJSarea", //svg:g class (D3 maps)
+	legendclass: "VisualJSlegend", //svg:g class (D3 maps)
+	normal: "VisualJSnormal", //visualitzation's normal size class
+	mini: "VisualJSmini", //visualitzation's small size class
+	colorclassprefix: "c", //prefix for color class in maps: c0, c1, c2...
+
+	//Markup created by visual
+	html: {
+		heading: "h1",
+		footer: "p" //"footer" element not supported by IE8
+	},
+
+	//Libraries: path and existence function
+	main: { //Do not use relative paths for main files in production: they'll be relative to the path where VisualJS.iframe is executed.
+		visual: "http://idescat.github.io/visual/visual.js",
+		setup: "http://idescat.github.io/visual/visual.setup.js",
+		lazy: "http://idescat.github.io/visual/lib/lazyload.js"
+	},
+	lib: {
+		d3: {
+			js: "http://idescat.github.io/visual/lib/d3.v3.js",
+			exists: function(){ return typeof d3==="object"; }
+		},
+		jquery: {
+			js: "http://idescat.github.io/visual/lib/jquery.1.8.3.js",
+			exists: function(){ return typeof jQuery==="function"; },
+
+			flot: {
+				js: "http://idescat.github.io/visual/lib/jquery.flot.js",
+				exists: function(){ return typeof jQuery.plot==="function"; },
+
+				stack: {
+					js: "http://idescat.github.io/visual/lib/jquery.flot.stack.js",
+					exists: function(){ return typeof jQuery.plot.plugins==="object" && typeof jQuery.plot.plugins[0]==="object" && jQuery.plot.plugins[0].name==="stack";  }//De moment no s'utilitza simult�niament amb cap altre plugin de Flot per tant �s el primer.
+				},
+				orderbars: {
+					js: "http://idescat.github.io/visual/lib/jquery.flot.orderbars.js",
+					exists: function(){ return typeof jQuery.plot.plugins==="object" && typeof jQuery.plot.plugins[0]==="object" && jQuery.plot.plugins[0].name==="orderBars"; }//De moment no s'utilitza simult�niament amb cap altre plugin de Flot per tant �s el primer.
+				},
+				pyramid: {
+					js: "http://idescat.github.io/visual/lib/jquery.flot.pyramid.js",
+					exists: function(){ return typeof FlotPyramid==="object"; }
+				},
+				categories: {
+					js: "http://idescat.github.io/visual/lib/jquery.flot.categories.js",
+					exists: function(){ return typeof jQuery.plot.plugins==="object" && typeof jQuery.plot.plugins[0]==="object" && jQuery.plot.plugins[0].name==="categories"; } //De moment no s'utilitza simult�niament amb cap altre plugin de Flot per tant �s el primer.
+				}
+			}
+		},
+		maps: {
+			js: "http://idescat.github.io/visual/maps/visual.maps.js",
+			exists: function(){ return typeof VisualJS.func.colors==="function" && typeof VisualJS.func.legend==="function";}
+		},
+		excanvas: {
+			js: "http://idescat.github.io/visual/lib/excanvas.js",
+			exists: function(){ return typeof G_vmlCanvasManager!=="undefined"; }
+		}
+	},
+
+	//Maps: path and existence function
+	map: {
+		mun: {
+			js: "http://idescat.github.io/visual/maps/cat2013mun.js",
+			exists: function(){ return typeof VisualJS.map.mun!=="undefined"; }
+		},
+		com: {
+			js: "http://idescat.github.io/visual/maps/cat2013com.js",
+			exists: function(){ return typeof VisualJS.map.com!=="undefined"; }
+		},
+		prov: {
+			js: "http://idescat.github.io/visual/maps/cat2013prov.js",
+			exists: function(){ return typeof VisualJS.map.prov!=="undefined"; }
+		},
+		usastates: {
+			js: "http://idescat.github.io/visual/maps/usa2013states.js",
+			exists: function(){ return typeof VisualJS.map.usastates!=="undefined"; }
+		},
+		com01: {
+			js: "http://idescat.github.io/visual/maps/com012013mun.js",
+			exists: function(){ return typeof VisualJS.map.com01!=="undefined"; }
+		},
+		com02: {
+			js: "http://idescat.github.io/visual/maps/com022013mun.js",
+			exists: function(){ return typeof VisualJS.map.com02!=="undefined"; }
+		},
+		com03: {
+			js: "http://idescat.github.io/visual/maps/com032013mun.js",
+			exists: function(){ return typeof VisualJS.map.com03!=="undefined"; }
+		},
+		com04: {
+			js: "http://idescat.github.io/visual/maps/com042013mun.js",
+			exists: function(){ return typeof VisualJS.map.com04!=="undefined"; }
+		},
+		com05: {
+			js: "http://idescat.github.io/visual/maps/com052013mun.js",
+			exists: function(){ return typeof VisualJS.map.com05!=="undefined"; }
+		},
+		com06: {
+			js: "http://idescat.github.io/visual/maps/com062013mun.js",
+			exists: function(){ return typeof VisualJS.map.com06!=="undefined"; }
+		},
+		com07: {
+			js: "http://idescat.github.io/visual/maps/com072013mun.js",
+			exists: function(){ return typeof VisualJS.map.com07!=="undefined"; }
+		},
+		com08: {
+			js: "http://idescat.github.io/visual/maps/com082013mun.js",
+			exists: function(){ return typeof VisualJS.map.com08!=="undefined"; }
+		},
+		com09: {
+			js: "http://idescat.github.io/visual/maps/com092013mun.js",
+			exists: function(){ return typeof VisualJS.map.com09!=="undefined"; }
+		},
+		com10: {
+			js: "http://idescat.github.io/visual/maps/com102013mun.js",
+			exists: function(){ return typeof VisualJS.map.com10!=="undefined"; }
+		},
+		com11: {
+			js: "http://idescat.github.io/visual/maps/com112013mun.js",
+			exists: function(){ return typeof VisualJS.map.com11!=="undefined"; }
+		},
+		com12: {
+			js: "http://idescat.github.io/visual/maps/com122013mun.js",
+			exists: function(){ return typeof VisualJS.map.com12!=="undefined"; }
+		},
+		com13: {
+			js: "http://idescat.github.io/visual/maps/com132013mun.js",
+			exists: function(){ return typeof VisualJS.map.com13!=="undefined"; }
+		},
+		com14: {
+			js: "http://idescat.github.io/visual/maps/com142013mun.js",
+			exists: function(){ return typeof VisualJS.map.com14!=="undefined"; }
+		},
+		com15: {
+			js: "http://idescat.github.io/visual/maps/com152013mun.js",
+			exists: function(){ return typeof VisualJS.map.com15!=="undefined"; }
+		},
+		com16: {
+			js: "http://idescat.github.io/visual/maps/com162013mun.js",
+			exists: function(){ return typeof VisualJS.map.com16!=="undefined"; }
+		},
+		com17: {
+			js: "http://idescat.github.io/visual/maps/com172013mun.js",
+			exists: function(){ return typeof VisualJS.map.com17!=="undefined"; }
+		},
+		com18: {
+			js: "http://idescat.github.io/visual/maps/com182013mun.js",
+			exists: function(){ return typeof VisualJS.map.com18!=="undefined"; }
+		},
+		com19: {
+			js: "http://idescat.github.io/visual/maps/com192013mun.js",
+			exists: function(){ return typeof VisualJS.map.com19!=="undefined"; }
+		},		
+		com20: {
+			js: "http://idescat.github.io/visual/maps/com202013mun.js",
+			exists: function(){ return typeof VisualJS.map.com20!=="undefined"; }
+		},
+		com21: {
+			js: "http://idescat.github.io/visual/maps/com212013mun.js",
+			exists: function(){ return typeof VisualJS.map.com21!=="undefined"; }
+		},
+		com22: {
+			js: "http://idescat.github.io/visual/maps/com222013mun.js",
+			exists: function(){ return typeof VisualJS.map.com22!=="undefined"; }
+		},
+		com23: {
+			js: "http://idescat.github.io/visual/maps/com232013mun.js",
+			exists: function(){ return typeof VisualJS.map.com23!=="undefined"; }
+		},
+		com24: {
+			js: "http://idescat.github.io/visual/maps/com242013mun.js",
+			exists: function(){ return typeof VisualJS.map.com24!=="undefined"; }
+		},
+		com25: {
+			js: "http://idescat.github.io/visual/maps/com252013mun.js",
+			exists: function(){ return typeof VisualJS.map.com25!=="undefined"; }
+		},
+		com26: {
+			js: "http://idescat.github.io/visual/maps/com262013mun.js",
+			exists: function(){ return typeof VisualJS.map.com26!=="undefined"; }
+		},
+		com27: {
+			js: "http://idescat.github.io/visual/maps/com272013mun.js",
+			exists: function(){ return typeof VisualJS.map.com27!=="undefined"; }
+		},
+		com28: {
+			js: "http://idescat.github.io/visual/maps/com282013mun.js",
+			exists: function(){ return typeof VisualJS.map.com28!=="undefined"; }
+		},
+		com29: {
+			js: "http://idescat.github.io/visual/maps/com292013mun.js",
+			exists: function(){ return typeof VisualJS.map.com29!=="undefined"; }
+		},
+		com30: {
+			js: "http://idescat.github.io/visual/maps/com302013mun.js",
+			exists: function(){ return typeof VisualJS.map.com30!=="undefined"; }
+		},
+		com31: {
+			js: "http://idescat.github.io/visual/maps/com312013mun.js",
+			exists: function(){ return typeof VisualJS.map.com31!=="undefined"; }
+		},
+		com32: {
+			js: "http://idescat.github.io/visual/maps/com322013mun.js",
+			exists: function(){ return typeof VisualJS.map.com32!=="undefined"; }
+		},
+		com33: {
+			js: "http://idescat.github.io/visual/maps/com332013mun.js",
+			exists: function(){ return typeof VisualJS.map.com33!=="undefined"; }
+		},
+		com34: {
+			js: "http://idescat.github.io/visual/maps/com342013mun.js",
+			exists: function(){ return typeof VisualJS.map.com34!=="undefined"; }
+		},
+		com35: {
+			js: "http://idescat.github.io/visual/maps/com352013mun.js",
+			exists: function(){ return typeof VisualJS.map.com35!=="undefined"; }
+		},
+		com36: {
+			js: "http://idescat.github.io/visual/maps/com362013mun.js",
+			exists: function(){ return typeof VisualJS.map.com36!=="undefined"; }
+		},
+		com37: {
+			js: "http://idescat.github.io/visual/maps/com372013mun.js",
+			exists: function(){ return typeof VisualJS.map.com37!=="undefined"; }
+		},
+		com38: {
+			js: "http://idescat.github.io/visual/maps/com382013mun.js",
+			exists: function(){ return typeof VisualJS.map.com38!=="undefined"; }
+		},
+		com39: {
+			js: "http://idescat.github.io/visual/maps/com392013mun.js",
+			exists: function(){ return typeof VisualJS.map.com39!=="undefined"; }
+		},
+		com40: {
+			js: "http://idescat.github.io/visual/maps/com402013mun.js",
+			exists: function(){ return typeof VisualJS.map.com40!=="undefined"; }
+		},
+		com41: {
+			js: "http://idescat.github.io/visual/maps/com412013mun.js",
+			exists: function(){ return typeof VisualJS.map.com41!=="undefined"; }
+		},
+		prov08: {
+			js: "http://idescat.github.io/visual/maps/prov082013mun.js",
+			exists: function(){ return typeof VisualJS.map.prov08!=="undefined"; }
+		},
+		prov17: {
+			js: "http://idescat.github.io/visual/maps/prov172013mun.js",
+			exists: function(){ return typeof VisualJS.map.prov17!=="undefined"; }
+		},
+		prov25: {
+			js: "http://idescat.github.io/visual/maps/prov252013mun.js",
+			exists: function(){ return typeof VisualJS.map.prov25!=="undefined"; }
+		},
+		prov43: {
+			js: "http://idescat.github.io/visual/maps/prov432013mun.js",
+			exists: function(){ return typeof VisualJS.map.prov43!=="undefined"; }
+		}		
+		
+	},
+
+	//IE check
+	func: {
+		old: function(ie) { return RegExp("(^|\\s)lt-"+ie+"(\\s|$)").test(document.documentElement.className); }
+	},
+
+	//Margins and paddings used in container
+	margin: 10,
+	padding: {
+		w: 30,
+		h: 45
+	},
+	//VisualJS.compare separator width
+	separator: 0
+};
