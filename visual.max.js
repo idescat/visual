ef903a5201edf8cf2069fa8e8099e61ec9617dbf
Xavier Badosa <xbadosa@gmail.com>
2015-03-02 16:22:21 +0100

    0.10.5
    autoheading was "misspelled" as heading somewhere.

--- visual.max.js
+++ visual.max.js
@@ -25,7 +25,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 /*global d3, LazyLoad*/
 var VisualJS={
-	version: "0.10.4",
+	version: "0.10.5",
 	show: true, //To be used when a callback function is specified: "false" means "don't run VisualJS.chart()", that is, load everything but don't draw.
 	old: false, //You can change it to true programmatically if you already know the browser is IE<9
 	fixed: null,
@@ -339,7 +339,7 @@ var VisualJS={
 				["listen","boolean",vsetup],
 				//scanvas
 				["dec","number",scanvas], 
-				["heading","boolean",scanvas], 
+				["autoheading","boolean",scanvas], 
 				["legend","boolean",scanvas], 
 				["grid","object",scanvas], 
 				[["grid","border"],"number",scanvas], 
